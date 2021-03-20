eec93ce316ffedf5b8aa7a2d7ba37f8b1d462ece
roridev <t3ctotalmenterandom1@outlook.com>
2021-03-19 21:17:37 -0300

    [Test Client] Ports to Mee6Client instead of explicitly calling the Request

--- Mee6Api.Test/Program.cs
+++ Mee6Api.Test/Program.cs
@@ -6,9 +6,9 @@ using Mee6Api.Requests;
 namespace Mee6Api.Test {
     class Program {
         static async Task Main(string[] args) {
-            using var client = new HttpClient();
-            GetLeaderboardRequest request = new(194925640888221698, page:1);
-            var leaderboard =  await request.GetAsync(client);
+            using var client = new Mee6Client();
+            var leaderboard = await client.GetLeaderboardAsync(194925640888221698);
+            var members = await client.GetAllMembersAsync(194925640888221698);
         }
     }
 }
